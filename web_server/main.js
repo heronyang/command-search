71d72d44a0f3fdbdb96db02f7a5ac5f7226608e8
Heron Yang <heron.yang.tw@gmail.com>
2019-06-08 11:40:18 -0700

    Adds entry of the newly added file in sqlite db

--- web_server/main.js
+++ web_server/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const formidable = require('formidable');
 const sqlite3 = require('sqlite3').verbose();
+const util  = require('util');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -26,7 +27,7 @@ app.post('/histfile_upload', function (req, res){
     form.parse(req, function(err, fields, files) {
         console.log(fields);
         console.log(files.data.path);
-        insertHistfileMeta();
+        insertHistfileMeta(fields, files.data.path);
         res.send('OK\n');
     });
     form.on('file', function (name, file){
@@ -37,6 +38,19 @@ app.post('/histfile_upload', function (req, res){
 /*
  * Database
  */
+
+const sql_drop_if_exists_table = `DROP TABLE IF EXISTS histfiles`;
+const sql_create_table = `CREATE TABLE histfiles (
+    os TEXT,
+    username TEXT,
+    system_id TEXT,
+    upload_time TIMESTAMP,
+    filepath TEXT
+)`;
+const sql_insert_row = `INSERT INTO histfiles
+    (os, username, system_id, upload_time, filepath)
+    VALUES ("%s", "%s", "%s", CURRENT_TIMESTAMP, "%s")`;
+
 const db_path = process.env.DATABASE;
 const db = new sqlite3.Database(db_path, sqlite3.OPEN_READWRITE,
     (err) => {
@@ -44,23 +58,33 @@ const db = new sqlite3.Database(db_path, sqlite3.OPEN_READWRITE,
             console.error(err.message);
         }
         console.log('Connected to database.');
-        db.run('DROP TABLE IF EXISTS histfiles');
-        db.run("CREATE TABLE histfiles (info TIMESTAMP)");
-
-        console.log('Running server on port ' + port);
-        app.listen(port);
-    }
-);
-
-function insertHistfileMeta() {
-    db.serialize(() => {
-        db.run(`INSERT INTO histfiles VALUES(CURRENT_TIMESTAMP)`,
-            function(err) {
+        console.log('Setting up a clean table.');
+        db.run(sql_drop_if_exists_table, function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            db.run(sql_create_table, function(err) {
                 if (err) {
                     return console.log(err.message);
                 }
-                console.log('New histfile entry is logged.');
+                app.listen(port);
+                console.log('Running server on port ' + port);
+            });
+        });
+    }
+);
+
+function insertHistfileMeta(fields, filepath) {
+    const sql = util.format(sql_insert_row, fields.os, fields.username,
+        fields.system_id, filepath);
+    console.log(sql);
+    db.serialize(() => {
+        db.run(sql, function(err) {
+            if (err) {
+                return console.log(err.message);
             }
+            console.log('New histfile entry is logged.');
+        }
         );
     });
 }
